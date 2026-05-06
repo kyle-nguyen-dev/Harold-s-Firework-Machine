e651ddba8a7c77bf08ee01166d210a094cd92135
Kyle Nguyen <kylevpn629@gmail.com>
2026-05-06 11:40:02 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file main.cpp
+ * @author Anik Momtaz
+ *
+ * Main entry point for program
+ */
+#include "pch.h"
+#include "CanadianExperienceApp.h"
+
+wxIMPLEMENT_APP(CanadianExperienceApp);
